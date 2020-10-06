7c989390b6c857a531977c705ac0a240aa50ed9c
Naman Kumar <naman@outlook.in>
2020-10-07 01:05:26 +0530

    [SyncDiscordUsers] return promise from worker

--- Workers/SyncDiscordUsers/index.ts
+++ Workers/SyncDiscordUsers/index.ts
@@ -9,13 +9,15 @@ export class SyncDiscordUsers extends Worker {
   async run() {
     const users = await User.findAllUnSynced<User>();
 
-    return users.map(async (user) => {
-      if (user.deleted) {
-        // TODO(naman): Remove user flow
-        return;
-      }
+    return Promise.all(
+      users.map(async (user) => {
+        if (user.deleted) {
+          // TODO(naman): Remove user flow
+          return Promise.resolve(null);
+        }
 
-      return Sync.addUser(user);
-    });
+        return Sync.addUser(user);
+      }),
+    );
   }
 }
